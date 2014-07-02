f02cff062e91b9afe584f009a13b735ede089ff0
yar1k <yarik.just@gmail.com>
2014-07-02 13:09:14 -0700

    Update CRUD.scala

--- app/models/CRUD.scala
+++ app/models/CRUD.scala
@@ -8,6 +8,6 @@ import Database.dynamicSession
 object Helper {
   
   val dbs = Database.forDataSource(play.api.db.DB.getDataSource())
-  //def database = Database.forUrl("postgres://postgres:last:partizan@localhost:5432/testplay", driver = "org.postgresql.Driver")
+  //def database = Database.forUrl("postgres://postgres:pass@localhost:5432/testplay", driver = "org.postgresql.Driver")
   
 }
